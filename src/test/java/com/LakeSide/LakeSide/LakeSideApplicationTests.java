99e8d67b58952f7d717156e8149c9da0bf50c355
Aleksa Djekic <leki.djekic@gmail.com>
2025-07-31 22:44:03 +0200

    Initialized repository

---
+++ src/test/java/com/LakeSide/LakeSide/LakeSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LakeSide.LakeSide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakeSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
