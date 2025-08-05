fe546523dbd03a2afc4c1309f2722a824f4b43db
Aleksa Djekic <84620255+Al3k5a24@users.noreply.github.com>
2025-08-05 23:48:27 +0200

    Add Room function developed

--- src/main/java/com/LakeSide/LakeSide/controller/RoomController.java
+++ src/main/java/com/LakeSide/LakeSide/controller/RoomController.java
@@ -1,5 +1,30 @@
 package com.LakeSide.LakeSide.controller;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.LakeSide.LakeSide.model.Room;
+import com.LakeSide.LakeSide.response.roomResponse;
+import com.LakeSide.LakeSide.service.IRoomService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class RoomController {
+	
+	private IRoomService roomService;
     
+	public ResponseEntity<roomResponse> addNewRoom(
+			@RequestParam("photo") MultipartFile photo,
+			@RequestParam("roomType") String roomType,
+			@RequestParam("roomPrice") BigDecimal roomPrice) throws IOException{
+		Room savedRoom=roomService.addNewRoom(photo,roomType,roomPrice);
+		roomResponse response=new roomResponse(savedRoom.getId(),
+				savedRoom.getRoomType(),savedRoom.getRoomPrice());
+		return ResponseEntity.ok(response);
+	}
 }
--- src/main/java/com/LakeSide/LakeSide/service/RoomServiceImpl.java
+++ src/main/java/com/LakeSide/LakeSide/service/RoomServiceImpl.java
@@ -1,5 +1,34 @@
 package com.LakeSide.LakeSide.service;
 
-public class RoomServiceImpl {
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.LakeSide.LakeSide.model.Room;
+import com.LakeSide.LakeSide.repository.RoomRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class RoomServiceImpl implements IRoomService{
+	
+	private RoomRepository roomRepository;
+
+	@Override
+	public Room addNewRoom(MultipartFile file,
+			String roomType,
+			BigDecimal roomPrice) throws IOException {
+		Room room=new Room();
+		if(!file.isEmpty()) {
+			byte[] photoByte=file.getBytes();
+			room.setPhoto(photoByte);
+		}
+		room.setRoomPrice(roomPrice);
+		room.setRoomType(roomType);
+		
+		return roomRepository.save(room);
+		
+	}
     
 }
